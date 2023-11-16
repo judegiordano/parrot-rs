d94fd325c3c4b5a79749a1e19da74a48d6b079c9
judegiordano <judegiordano@gmail.com>
2023-11-16 16:08:36 -0500

    Remove Record Model

--- src/bin/scripts/migrate-indexes.rs
+++ src/bin/scripts/migrate-indexes.rs
@@ -1,13 +1,10 @@
 use anyhow::Result;
-use parrot_api::{
-    logger,
-    models::{record::Record, voice::Voice},
-};
+use parrot_api::{logger, models::voice::Voice};
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
     logger::init()?;
-    let results = futures::try_join!(Record::migrate(), Voice::migrate())?;
+    let results = futures::try_join!(Voice::migrate())?;
     tracing::info!("{:#?}", results);
     Ok(())
 }
