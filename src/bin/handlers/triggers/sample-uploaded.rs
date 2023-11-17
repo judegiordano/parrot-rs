3e38c81162d4161fa4b3e7f7d55639b0c41632b2
judegiordano <judegiordano@gmail.com>
2023-11-17 13:19:51 -0500

    Error Handling

--- src/bin/handlers/triggers/sample-uploaded.rs
+++ src/bin/handlers/triggers/sample-uploaded.rs
@@ -10,23 +10,29 @@ use parrot_api::{
 };
 
 async fn handler(event: LambdaEvent<S3Event>) -> Result<()> {
-    tracing::info!("receiving objects {:#?}", event.payload.records);
+    tracing::info!("[RECEIVING OBJECTS] {:?}", event.payload.records);
     for record in event.payload.records {
-        let key = &record.s3.object.key.unwrap();
+        let key = match &record.s3.object.key {
+            Some(key) => key,
+            None => anyhow::bail!("no key exists on object"),
+        };
         let split = key.split(".mp3").collect::<Vec<_>>();
-        let voice_id = split.first().unwrap();
-        // todo handle errors
-        let bucket = record.s3.bucket.name.unwrap();
+        let voice_id = match split.first() {
+            Some(str) => *str,
+            None => anyhow::bail!("missing file name on split key"),
+        };
+        let bucket = match record.s3.bucket.name {
+            Some(bucket) => bucket,
+            None => anyhow::bail!("no bucket exists on object"),
+        };
         let sample_bucket = Client::new(&bucket).await;
-        tracing::info!("[KEY]: {:#?}", key);
-        tracing::info!("[VOICE_ID]: {:#?}", voice_id);
-        tracing::info!("[BUCKET]: {:#?}", bucket);
         let sample = sample_bucket.get_object(key.to_string()).await?;
-        tracing::info!("[SAMPLE]: {:#?}", sample);
         let api = ElevenLabs::new()?;
-        let voice = Voice::read_by_id(&voice_id).await?;
+        let voice = Voice::read_by_id(voice_id).await?;
         let data = sample.body.collect().await?.to_vec();
-        let cloned_voice = api.add_voice(&voice.name, &data, None).await?;
+        let cloned_voice = api
+            .add_voice(&voice.name, &data, voice.description.as_deref())
+            .await?;
         let updated_voice = Voice::update(
             doc! { "_id": voice.id },
             doc! {
@@ -35,7 +41,7 @@ async fn handler(event: LambdaEvent<S3Event>) -> Result<()> {
             },
         )
         .await?;
-        tracing::info!("cloned voice {:#?}", updated_voice);
+        tracing::info!("VOICE {:?}", updated_voice);
     }
     Ok(())
 }
