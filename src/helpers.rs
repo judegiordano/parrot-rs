4ba4c43a26c3ec4b0d1f9bb12d15ac67a37636b5
judegiordano <judegiordano@gmail.com>
2023-11-18 11:34:34 -0500

    Split Bearer Token

--- src/helpers.rs
+++ src/helpers.rs
@@ -4,10 +4,15 @@ use crate::env;
 
 pub async fn authenticate(req: HttpRequest) -> anyhow::Result<()> {
     let config = env::Config::new()?;
-    let token = match req.headers().get("Authorization") {
+    let bearer_token = match req.headers().get("Authorization") {
         Some(value) => value.to_str(),
         None => anyhow::bail!("missing authentication header"),
     }?;
+    let parts = bearer_token.split(' ').collect::<Vec<_>>();
+    let token = match parts.get(1) {
+        Some(token) => *token,
+        None => anyhow::bail!("missing authentication token"),
+    };
     if token != config.authentication_token {
         anyhow::bail!("invalid authentication token")
     }
